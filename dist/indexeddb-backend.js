111f86957ac26befba7b277668bb7958c897ce23
Sergey Popov <quolpr@gmail.com>
2021-09-01 16:50:04 +0300

    Update dist

--- dist/indexeddb-backend.js
+++ dist/indexeddb-backend.js
@@ -1013,6 +1013,27 @@ class FileOps {
   }
 }
 
+/**
+ * https://bugs.webkit.org/show_bug.cgi?id=226547
+ * Safari has a horrible bug where IDB requests can hang while the browser is starting up.
+ * The only solution is to keep nudging it until it's awake.
+ * This probably creates garbage, but garbage is better than totally failing.
+ */
+function idbReady() {
+    const isSafari = !navigator.userAgentData &&
+        /Safari\//.test(navigator.userAgent) &&
+        !/Chrom(e|ium)\//.test(navigator.userAgent);
+    // No point putting other browsers or older versions of Safari through this mess.
+    if (!isSafari || !indexedDB.databases)
+        return Promise.resolve();
+    let intervalId;
+    return new Promise((resolve) => {
+        const tryIdb = () => indexedDB.databases().finally(resolve);
+        intervalId = setInterval(tryIdb, 100);
+        tryIdb();
+    }).finally(() => clearInterval(intervalId));
+}
+
 function positionToKey(pos, blockSize) {
   // We are forced to round because of floating point error. `pos`
   // should always be divisible by `blockSize`
@@ -1020,6 +1041,8 @@ function positionToKey(pos, blockSize) {
 }
 
 async function openDb(name) {
+  await idbReady();
+  
   return new Promise((resolve, reject) => {
     let req = globalThis.indexedDB.open(name, 2);
     req.onsuccess = event => {
